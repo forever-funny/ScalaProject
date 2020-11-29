08da5b9f5db0152e09aab7846a0a6ffc30cc72f6
李龙 <1306205691@qq.com>
2020-11-29 22:46:10 +0800

    First commit

---
+++ Demo/src/com/googler/demo/NewMain.scala
@@ -0,0 +1,16 @@
+package com.googler.demo
+
+/**
+ * @author : Administrator
+ * @date : 2020-11-29 0029 22:41
+ *       Description : 
+ * */
+object NewMain {
+  def main(args: Array[String]): Unit = {
+    val i = 6
+    println(i * 10_000)
+    val s = "hello"
+    val str: String = "World "
+    print(str.concat(s))
+  }
+}
